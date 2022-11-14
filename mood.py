28766ba2945e2e73c245933759f647ea0312a547
Christian Benstein <Christian.Benstein@Topicus.nl>
2022-11-14 10:28:57 +0100

    Added moods class

--- mood.py
+++ mood.py
@@ -2,27 +2,26 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import InputRequired, Length, DataRequired
 
+class Moods():
+    def __init__(self):
+        self.Fantastic = {"description": "Fantastic", "color": "succes", "icon": "🤩"}
+        self.Great = {"description": "Great", "color": "info", "icon": "😄"}
+        self.Good = {"description": "Good", "color": "light", "icon": "🙂"}
+        self.Bad = {"description": "Bad", "color": "warning", "icon": "🙁"}
+        self.Terrible = {"description": "Terrible", "color": "danger", "icon": "😡"}
 
-moods = { 
-            "Fantastic": {"description": "Fantastic", "color": "succes", "icon": "🤩"},
-            "Great": {"description": "Great", "color": "info", "icon": "😄"},
-            "Good": {"description": "Good", "color": "light", "icon": "🙂"},
-            "Bad": {"description": "Bad", "color": "warning", "icon": "🙁"},
-            "Terrible": {"description": "Terrible", "color": "danger", "icon": "😡"}
-        }  
+    def moodColor(self, mood):
+        return self.mood['color']
 
+    def moodSmiley(self, mood):
+        return self.mood['icon']
+        
 class Mood():
     def __init__(self, name, mood, comment):
         self.mood = mood
         self.name = name
         self.comment = comment
 
-    def moodColor(self):
-        return moods[self.mood]['color']
-
-    def moodSmiley(self):
-        return moods[self.mood]['icon']
- 
 
 class mood_form(FlaskForm):
     name = StringField("TextLabel")
